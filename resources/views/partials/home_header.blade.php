7d0428f9c2f3865cceb8174aa01d1f8113617887
Nice Web Technologies <nicewebtechnologies@gmail.com>
2024-11-09 00:17:07 +0530

    icon

--- resources/views/partials/home_header.blade.php
+++ resources/views/partials/home_header.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Nice Computer Institute &#8211; Be a Part Of I.T</title>
-    <link rel="icon" href="http://webdesign.nicewebtech.com/assets/images/logo_new_icon_nice_computer_institute_jahangirpuri.png" type="image/x-icon">
+    <link rel="icon" href="https://webdesign.nicewebtech.com/assets/images/logo_new_icon_nice_computer_institute_jahangirpuri.png" type="image/x-icon">
     <link rel="stylesheet" href="{{ asset('assets/bootstrap5/bootstrap-5.0.2-dist/css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
