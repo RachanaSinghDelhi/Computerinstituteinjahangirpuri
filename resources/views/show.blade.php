6cba086626ea5e36b3ba428921f7d0124c4b7a6c
Nice Web Technologies <nicewebtechnologies@gmail.com>
2024-12-23 10:28:07 +0530

    commit

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,9 +1,9 @@
 @section('title', "$course->course_title")
 
 @include('partials.course_header',['courses' => $courses])
-
+<!-- {{ asset('assets/images/Sliders_image/medal_nice_computer_institute_jahangirpuri.jpg') }}-->
 <!--style="background-image: url('{{ asset('storage/courses/' . $course->course_image) }}'-->
-<div class="banner" style="background-image: url('{{ asset('assets/images/Sliders_image/medal_nice_computer_institute_jahangirpuri.jpg') }}'); height: 400px; background-size: cover; background-position: center;">
+<div class="banner" style="background-image: url('{{ asset('storage/courses/' . $course->course_image) }}'); height: 400px; background-size: cover; background-position: center;">
         <div class="overlay" style="background-color: rgba(0, 0, 0, 0.5); height: 100%; display: flex; justify-content: center; align-items: center;">
         <div class="container-fluid bg-breadcrumb">
 <nav aria-label="breadcrumb">
