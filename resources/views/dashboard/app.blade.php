c9531d7fe228aebf130c84e1e49fbc94f36fd0a6
Nice Web Technologies <nicewebtechnologies@gmail.com>
2025-01-19 19:21:54 +0530

    commit

--- resources/views/dashboard/app.blade.php
+++ resources/views/dashboard/app.blade.php
@@ -1,46 +1,47 @@
+<!-- resources/views/dashboard/app.blade.php -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <!-- Head Content -->
+    <meta charset="UTF-8">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title', 'Nice Computer Institute')</title>
+    <link rel="icon" href="https://www.computerinstituteindelhijahangirpuri.com/assets/images/nice.ico" type="image/x-icon">
+    <link rel="stylesheet" href="{{ asset('assets/bootstrap5/bootstrap-5.0.2-dist/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/style2.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+  
+     <!-- Cropper.js CSS -->
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.12/cropper.min.css" />
 </head>
-<body>
-    <div class="d-flex flex-column min-vh-100">
-    <header>
-    @include('dashboard.dash_header')
-    <div class="banner" style="
-        background-image: url('{{ asset('assets/images/Advance_excel_nice_computer_institute_jahangirpuri.jpg') }}');
-        height: 150px;
-        background-size: cover;
-        background-position: center;
-        position: relative;
-    ">
-        <div class="overlay" style="
-            background-color: rgba(0, 0, 0, 0.5);
-            height: 100%;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-        ">
-            <h1 class="banner-title">@yield('title')</h1>
+<body class="d-flex flex-column" style="height: 100vh;"> 
+
+    <!-- Wrapper to hold sidebar and content -->
+    <div class="d-flex flex-grow-1">
+
+        <!-- Sidebar -->
+        @include('dashboard.sidebar')
+
+        <!-- Main Content Area -->
+        <div class="flex-grow-1 d-flex flex-column">
+            
+            <!-- Topbar -->
+            @include('dashboard.topbar')
+
+            <!-- Main Content -->
+            <main class="container-fluid flex-grow-1 pt-4" style="margin-top: 50px; margin-left: 20px;">
+                @yield('content')
+            </main>
+            
         </div>
     </div>
-</header>
 
+    <!-- Footer -->
+    @include('dashboard.footer')
 
-       
-            @yield('content')
-     
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"></script>
 
-
-            @include('dashboard.dash_footer')
-        
-    </div>
-
-    @stack('scripts')
-    <!-- Scripts -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.13/cropper.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-    <script src="{{ asset('assets/bootstrap5/bootstrap-5.0.2-dist/js/bootstrap.bundle.min.js') }}"></script>
-    <script src="{{ asset('assets/js/script.js') }}"></script>
-</body>
+   </body>
 </html>
---
+++ resources/views/dashboard/sidebar.blade.php
@@ -0,0 +1,156 @@
+<div class="d-flex" id="wrapper">
+    <!-- Sidebar -->
+    <div class="bg-dark text-white" id="sidebar" style="width: 250px; height: 100%; transition: all 0.3s;">
+        <nav class="nav flex-column p-3">
+            <a href="/" class="nav-link mb-3">
+                <img src="{{ asset('assets/images/logo1_3D_half.png') }}" alt="Logo" width="150" style="margin-top:30px;">
+            </a>
+            <a href="{{ route('course.index') }}" class="nav-link text-white">
+                <i class="fa fa-graduation-cap"></i> <span>Dashbaord</span>
+            </a>
+
+            <!-- Divider -->
+            <hr class="text-white">
+
+            <!-- ToggleDown Menu for Courses -->
+            <a href="#courseMenu" class="nav-link text-white" data-bs-toggle="collapse" aria-expanded="false" aria-controls="courseMenu">
+                <i class="fa fa-book"></i> <span>Courses</span>
+                <i class="fa fa-chevron-down ms-2 arrow"></i>
+            </a>
+            <div class="collapse" id="courseMenu">
+                <a href="{{ route('course.create') }}" class="nav-link text-white ms-3">
+                    <i class="fa fa-plus"></i> <span>Add Course</span>
+                </a>
+                <a href="{{ route('course.index') }}" class="nav-link text-white ms-3">
+                    <i class="fa fa-list"></i> <span>Course List</span>
+                </a>
+            </div>
+
+            <!-- Divider -->
+            <hr class="text-white">
+
+            <!-- ToggleDown Menu for Posts -->
+            <a href="#postMenu" class="nav-link text-white" data-bs-toggle="collapse" aria-expanded="false" aria-controls="postMenu">
+                <i class="fa fa-pencil"></i> <span>Posts</span>
+                <i class="fa fa-chevron-down ms-2 arrow"></i>
+            </a>
+            <div class="collapse" id="postMenu">
+                <a href="{{ route('posts.create_post') }}" class="nav-link text-white ms-3">
+                    <i class="fa fa-plus"></i> <span>Add Post</span>
+                </a>
+                <a href="{{ route('posts.new_posts') }}" class="nav-link text-white ms-3">
+                    <i class="fa fa-list"></i> <span>Post List</span>
+                </a>
+            </div>
+
+            <!-- Divider -->
+            <hr class="text-white">
+
+            <!-- ToggleDown Menu for Students -->
+            <a href="#studentMenu" class="nav-link text-white" data-bs-toggle="collapse" aria-expanded="false" aria-controls="studentMenu">
+                <i class="fa fa-users"></i> <span>Students</span>
+                <i class="fa fa-chevron-down ms-2 arrow"></i>
+            </a>
+            <div class="collapse" id="studentMenu">
+                <a href="{{ route('students.create') }}" class="nav-link text-white ms-3">
+                    <i class="fa fa-user-plus"></i> <span>Add Student</span>
+                </a>
+                <a href="{{ route('student.index') }}" class="nav-link text-white ms-3">
+                    <i class="fa fa-users"></i> <span>Ajax Student</span>
+                </a>
+                <a href="{{ route('students.index') }}" class="nav-link text-white ms-3">
+                    <i class="fa fa-list"></i> <span>Student List</span>
+                </a>
+                <a href="{{ route('students.id-cards') }}" class="nav-link text-white ms-3">
+                    <i class="fa fa-id-card"></i> <span>ID Cards</span>
+                </a>
+            </div>
+
+            <!-- Divider -->
+            <hr class="text-white">
+
+            <!-- ToggleDown Menu for Fees -->
+            <a href="#feesMenu" class="nav-link text-white" data-bs-toggle="collapse" aria-expanded="false" aria-controls="feesMenu">
+                <i class="fa fa-money"></i> <span>Fees</span>
+                <i class="fa fa-chevron-down ms-2 arrow"></i>
+            </a>
+            <div class="collapse" id="feesMenu">
+                <a href="{{ route('fees.index') }}" class="nav-link text-white ms-3">
+                    <i class="fa fa-money-check"></i> <span>Fees</span>
+                </a>
+            </div>
+
+            <!-- Divider -->
+            <hr class="text-white">
+
+            <!-- ToggleDown Menu for Certificates -->
+            <a href="#certificateMenu" class="nav-link text-white" data-bs-toggle="collapse" aria-expanded="false" aria-controls="certificateMenu">
+                <i class="fa fa-certificate"></i> <span>Certificates</span>
+                <i class="fa fa-chevron-down ms-2 arrow"></i>
+            </a>
+            <div class="collapse" id="certificateMenu">
+                <a href="{{ route('certificates.index') }}" class="nav-link text-white ms-3">
+                    <i class="fa fa-certificate"></i> <span>Certificates</span>
+                </a>
+                <a href="{{ route('certificates.select') }}" class="nav-link text-white ms-3">
+                    <i class="fa fa-check"></i> <span>Select Certificates</span>
+                </a>
+            </div>
+
+            <form action="{{ route('admin.logout') }}" method="POST" class="mt-3">
+                @csrf
+                <button type="submit" class="btn btn-danger w-100">Logout</button>
+            </form>
+        </nav>
+    </div>
+
+    <!-- Content Wrapper -->
+    <div id="page-content-wrapper">
+        <!-- Toggle Button -->
+        <button id="toggleSidebar" class="btn btn-secondary position-fixed" style="top: 20px; left: 20px; z-index: 10;">
+            <i class="fa fa-bars"></i>
+        </button>
+        
+        <!-- Main content goes here -->
+        <div class="container-fluid">
+            <!-- Your content here -->
+        </div>
+    </div>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+<script>
+    $(document).ready(function() {
+        // Handle the toggle-down functionality (unchanged)
+        $('.nav-link[data-bs-toggle="collapse"]').on('click', function(e) {
+            var targetMenu = $($(this).attr('href'));
+            var arrowIcon = $(this).find('.arrow');
+
+            // Close all other open menus
+            $('.collapse.show').not(targetMenu).collapse('hide');
+            $('.nav-link[data-bs-toggle="collapse"] .arrow').not(arrowIcon).removeClass('fa-chevron-up').addClass('fa-chevron-down');
+
+            // Toggle the clicked menu
+            targetMenu.collapse('toggle');
+
+            // Toggle the arrow icon for the current menu
+            if (targetMenu.hasClass('show')) {
+                arrowIcon.removeClass('fa-chevron-down').addClass('fa-chevron-up');
+            } else {
+                arrowIcon.removeClass('fa-chevron-up').addClass('fa-chevron-down');
+            }
+        });
+
+      
+    // Handle the sidebar toggle
+    $('#toggleSidebar').click(function() {
+        $('#sidebar').toggleClass('active');  // Toggle sidebar visibility
+        $('#page-content-wrapper').toggleClass('active');  // Adjust content area when sidebar is toggled
+    });
+
+
+    });
+</script>
+
+</script>
