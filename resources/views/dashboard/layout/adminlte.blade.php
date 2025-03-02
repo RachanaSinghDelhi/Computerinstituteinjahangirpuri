85971387d6ab895a534bbaf66a57942329bdf015
Rachana singh <evesdrop08@gmail.com>
2025-03-02 10:08:50 +0530

    commit

--- resources/views/dashboard/layout/adminlte.blade.php
+++ resources/views/dashboard/layout/adminlte.blade.php
@@ -37,6 +37,9 @@
     <!-- AdminLTE Scripts -->
     <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
     <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <!-- Add this inside the <head> or before </body> -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
     <script src="{{ asset('vendor/adminlte/dist/js/adminlte.min.js') }}"></script>
 
     @stack('js')
