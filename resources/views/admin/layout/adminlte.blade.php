f2492a2b8dd2e6744119133af7b4030d1558a222
Rachana singh <evesdrop08@gmail.com>
2025-03-02 11:18:45 +0530

    commit

--- resources/views/admin/layout/adminlte.blade.php
+++ resources/views/admin/layout/adminlte.blade.php
@@ -6,6 +6,8 @@
     <title>@yield('title', 'Admin Dashboard')</title>
 
     <!-- AdminLTE Styles -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 
     <link rel="stylesheet" href="{{ asset('vendor/adminlte/dist/css/adminlte.min.css') }}">
--- resources/views/dashboard/layout/adminlte.blade.php
+++ resources/views/dashboard/layout/adminlte.blade.php
@@ -4,9 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Admin Dashboard')</title>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <!-- AdminLTE Styles -->
+   
+
     <link rel="stylesheet" href="{{ asset('vendor/adminlte/dist/css/adminlte.min.css') }}">
     <link rel="stylesheet" href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}">
 
